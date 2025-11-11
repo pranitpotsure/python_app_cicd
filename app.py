46f7db8aa1a0e32480e09e0d1a367a23b8f77ec1
pranitpotsure <pranitpotsure55@gmail.com>
2025-11-11 11:34:36 +0530

    webhook

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return 'hello from pranit n huhkjhkjhuccessfully deployed python application through jenkins!!!!!!!!!, added webhook'
+    return 'zala kayyyyy  deployed python application through jenkins!!!!!!!!!, added webhook'
 @app.route('/hi')
 def hell():
     return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
